dcd567759d353d839da6d750435205fc2627a1b7
Alexandra Vabnits <a.vabnits@innopolis.university>
2023-11-20 22:12:09 +0300

    refactor

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def main():
         transportation = Transportation(supply, demand, costs)
         transportation.print_initial_table()
         solution = transportation.russell_method()
-        print(solution)
+        solution1 = transportation.northwest_corner_method()
+        solution2 = transportation.vogel_method()
+        print("Northwest Corner rule:\n", solution1)
+        print("Vogel's approximation:\n", solution2)
     except InfeasibleSolution:
         print("The problem does not have solution!")
         return
